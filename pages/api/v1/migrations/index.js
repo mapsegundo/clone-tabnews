29dc85b73858584e6bd998632522077efd09dae7
Marshall Paiva <marshallpaiva@hotmail.com>
2024-12-12 17:54:03 -0300

    fix `/migrations` endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,24 +3,32 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(req, res) {
-  const dbClient = await database.getNewClient();
-  const migrationsOptions = {
-    dbClient: dbClient,
-    databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(req.method)) {
+    return res
+      .status(405)
+      .json({ error: `Method "${req.method}" not allowed` });
+  }
+
+  let dbClient;
 
   try {
+    dbClient = await database.getNewClient();
+    const migrationsOptions = {
+      dbClient: dbClient,
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+
     if (req.method !== "POST") {
       const getMigrations = await migrationRunner({
         ...migrationsOptions,
         dryRun: true,
       });
-      await dbClient.end();
       return res.status(200).json(getMigrations);
     }
     if (req.method === "POST") {
@@ -28,12 +36,13 @@ export default async function migrations(req, res) {
         ...migrationsOptions,
         dryRun: false,
       });
-      await dbClient.end();
       return res.status(200).json(postMigrations);
     }
     return res.status(405).json({ error: "Method not allowed" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: error.message });
+  } finally {
+    await dbClient.end();
   }
 }
